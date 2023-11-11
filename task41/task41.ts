efa43073ae8122de0338d227cad08411b9a1ddbd
UsamaTanoli <usamakhan123451998@gmail.com>
2023-11-11 18:34:13 +0500

    show magicians

---
+++ task41/task41.ts
@@ -0,0 +1,10 @@
+let magicians:string[] = ["Harry Houdini", "David Blaine", "Doug Henning", "Dai Vernon"]
+
+function show_magicians(){
+    for(let magician of magicians){
+    console.log(magician);
+    }
+}
+
+show_magicians()
+
