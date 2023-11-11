8462427c22eafcefb9d85b1351f4b673db4ac7fb
UsamaTanoli <usamakhan123451998@gmail.com>
2023-11-11 16:13:10 +0500

    city , country

---
+++ task39/task39.js
@@ -0,0 +1,7 @@
+"use strict";
+function city_country(name, country) {
+    return `${name}, ${country}`;
+}
+console.log("Karachi", "Pakistan");
+console.log("Delhi", "India");
+console.log("Kabul", "Afghanistan");
---
+++ task39/task39.ts
@@ -0,0 +1,8 @@
+function city_country(city: string, country: string): string {
+    return `${city}, ${country}`;
+}
+
+console.log(city_country("Karachi", "Pakistan"));
+console.log(city_country("Delhi", "India"));
+console.log(city_country("Kabul", "Afghanistan"));
+
